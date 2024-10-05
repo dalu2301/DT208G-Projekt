93800b7a1d390471308d4238217276cdb1004ece
dalu2301 <dalu2301@student.miun.se>
2024-10-05 15:43:27 +0200

    Finished working on enhanced filtering, i.e. support both text and subject filter at the same time

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core'
 import { Course } from '../../models/course'
 import { CourseHandlerService } from '../../services/course-handler.service'
 import { CommonModule } from '@angular/common'
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
+
+enum SortOrder {
+  'Ascending' = 0,
+  'Descending' = 1
+}
 
 @Component({
   selector: 'app-courses',
@@ -17,6 +22,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 
 export class CoursesComponent implements OnInit {
 
+  // Diverse globala variabler
   filterFormGroup: FormGroup
   courses: Course[] = []
   coursesFiltered: Course[] = []
@@ -30,12 +36,15 @@ export class CoursesComponent implements OnInit {
 
     // Skapa ett Reactive Forms-formulär
     this.filterFormGroup = new FormGroup({
+
       filterInput: new FormControl(''),
       filterSelect: new FormControl('')
+
     })
 
   }
 
+  // Initieringslogik
   ngOnInit(): void {
 
     // Börjar med att hämta in data från JSON-fil
@@ -43,10 +52,15 @@ export class CoursesComponent implements OnInit {
 
       next: (courses) => {
 
+        // Originallistan ("facit")
         this.courses = courses
+        // Lista som kommer att filtreras
         this.coursesFiltered = courses
+        // Ämnesområden för select-element
         this.subjects = this.uniqueSubjects(this.courses)
+        // Information om antalet hittade kurser utifrån filtrering
         this.numberOfCoursesTotal = courses.length
+        this.numberOfCoursesCurrent = courses.length
 
       },
 
@@ -60,7 +74,12 @@ export class CoursesComponent implements OnInit {
 
   }
 
-  // Plockar ut unika ämnesområden för select-elementet
+  /**
+   * Plockar ut unika ämnesområden för Select-elementet.
+   * Prestandamässigt är det nog inte bästa alternativet
+   * att använda Map, men med en så pass lite datamångd
+   * som i detta fall, så fyller det sin funktion väl.
+   */
   uniqueSubjects(array: Course[]): string[] {
 
     const map = new Map()
@@ -74,19 +93,98 @@ export class CoursesComponent implements OnInit {
 
     }
 
-    // ToDo: Sortera alfabetiskt innan retur...
+    // TODO: fixa till sortering enligt Locale innan retur (åäö blir i fel ordning)
 
     // Konverterar Map till Array innan retur.
     return Array.from(map.values())
 
   }
 
-  filterCourses() {
-    console.log(this.filterFormGroup.value.filterInput)
-  }
+  /**
+   * Metoden tar hänsyn till om bara 1) fritextfiltrering, 2) filtrering
+   * via Select, eller om 3) både Select- och fritextfiltrering gjorts.
+   * 
+   * 1) Om ingen filtrering gjorts via Select, då utgår fritextfiltreringen
+   * från originallistan, det vill säga courses: Course[]
+   * 
+   * 2) Om ingen filtrering gjorts via fritext, då utgår Select-filtreringen
+   * från originallistan, det vill säga courses: Course[]
+   * 
+   * 3) Om både fritextfiltrering och Select-filtrering gjorts, då 
+   * börjar filtreringen med att läsa ut enligt Select, för att sedan
+   * filtrera igen via fritext på redan Select-filtrerade data.
+   */
+  filterCourses(): void {
 
-  filterSelect() {
-    console.log(this.filterFormGroup.value.filterSelect)
-  }
+    let filterInput: string = this.filterFormGroup.value.filterInput
+    let filterSelect: string = this.filterFormGroup.value.filterSelect
 
+    if (filterSelect.length === 0) {
+
+      // Ingen filtrering via Select är gjord,
+      // filtrera endast med hjälp av fritext.
+      if (filterInput.length > 0) {
+
+        this.coursesFiltered = this.courses.filter(course => {
+          return (
+            (course.courseName.toLowerCase().includes(filterInput.toLowerCase())) ||
+            (course.courseCode.toLocaleLowerCase().includes(filterInput.toLowerCase()))
+          )
+
+        })
+
+        this.numberOfCoursesCurrent = this.coursesFiltered.length
+
+      } else {
+
+        // Ingen fritextfiltrering är gjord i detta fall.
+        // Select är återställd, det vill säga 0 (value=""),
+        // så läs in hela listan igen.
+        this.coursesFiltered = this.courses
+        this.numberOfCoursesCurrent = this.courses.length
+
+      }
+
+    } else {
+
+      // Filtrering är gjord via Select. Kontrollera
+      // om det finns en fritextfiltrering också.
+      let coursesFilteredTwice: Course[] = []
+
+      if (filterInput.length > 0) {
+      
+        // Fritextfiltrering gjord. Börjar med att avgränsa 
+        // filtreringsmängden till det valda ämnesområdet.
+        coursesFilteredTwice = this.coursesFiltered = this.courses.filter(subject => {
+          return subject.subject.toLowerCase() === filterSelect.toLowerCase()
+        })
+
+        // Skapar sedan en lista utifrån fritextfiltrering också
+        // med hjälp av mellanlagrad data i variabeln courseFilteredTwice.
+        this.coursesFiltered = coursesFilteredTwice.filter(course => {
+          return (
+            (course.courseName.toLowerCase().includes(filterInput.toLowerCase())) ||
+            (course.courseCode.toLocaleLowerCase().includes(filterInput.toLowerCase()))
+          )
+
+        })
+
+        this.numberOfCoursesCurrent = this.coursesFiltered.length
+
+      } else {
+
+        // Ingen fritextfiltrering gjord. Returnera ett
+        // resultat endast baserat på filtrering via Select.
+        this.coursesFiltered = this.courses.filter(subject => {
+          return subject.subject.toLowerCase() === filterSelect.toLowerCase()
+        })
+  
+        this.numberOfCoursesCurrent = this.coursesFiltered.length
+
+      }
+
+    }
+
+  }
+  
 }
